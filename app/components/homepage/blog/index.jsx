c9930c97d0d6f9533f210b07bcb6bbabd16d7bdd
srivatsansreedaran <srivatsansreedaran1@gmail.com>
2025-08-31 12:29:46 +0530

    Removed blog section

--- app/components/homepage/blog/index.jsx
+++ app/components/homepage/blog/index.jsx
@@ -1,7 +1,7 @@
 // @flow strict
 import Link from 'next/link';
 import { FaArrowRight } from 'react-icons/fa';
-import BlogCard from './blog-card';
+// import BlogCard from './blog-card';
 
 function Blog({ blogs }) {
 
@@ -33,7 +33,7 @@ function Blog({ blogs }) {
           ))
         }
       </div>
-
+{/* 
       <div className="flex justify-center  mt-5 lg:mt-12">
         <Link
           className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-3 md:px-8 py-3 md:py-4 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
@@ -43,7 +43,7 @@ function Blog({ blogs }) {
           <span>View More</span>
           <FaArrowRight size={16} />
         </Link>
-      </div>
+      </div> */}
     </div>
   );
 };
